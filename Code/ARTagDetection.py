205f4feaa5b9abbb0d0b7223549e98a2adbc11b8
Sakshi Kakde <kakdesakshi@gmail.com>
2021-03-01 14:22:53 -0500

    tag detection, compute H, warp image, inverse warping

---
+++ Code/ARTagDetection.py
@@ -0,0 +1,208 @@
+import numpy as np
+import cv2
+import scipy
+import matplotlib.pyplot as plt
+from Utils.Utils import *
+
+def createCircularMask(image_size, radius, high_pass = True):
+    rows, cols = image_size
+    centre_x, centre_y = int(rows / 2), int(cols / 2)
+    center = [centre_x, centre_y]
+    x, y = np.ogrid[:rows, :cols]
+    mask_area = (x - center[0]) ** 2 + (y - center[1]) ** 2 <= radius*radius
+
+    if high_pass:
+        mask = np.ones((rows, cols)) 
+        mask[mask_area] = 0
+    else:
+        mask = np.zeros((rows, cols)) 
+        mask[mask_area] = 1
+
+    return mask
+
+def createCircularMask(image_size, radius, high_pass = True):
+    rows, cols = image_size
+    centre_x, centre_y = int(rows / 2), int(cols / 2)
+    center = [centre_x, centre_y]
+    x, y = np.ogrid[:rows, :cols]
+    mask_area = (x - center[0]) ** 2 + (y - center[1]) ** 2 <= radius*radius
+
+    if high_pass:
+        mask = np.ones((rows, cols)) 
+        mask[mask_area] = 0
+    else:
+        mask = np.zeros((rows, cols)) 
+        mask[mask_area] = 1
+
+    return mask
+
+def findContour(image, SavePath, i):
+    ret,thresh = cv2.threshold(np.uint8(image), 200 ,255,cv2.THRESH_BINARY)
+    plt.figure()
+    plt.imshow(thresh, cmap = 'gray')
+    plt.savefig(SavePath + "thresh" + str(i) + ".jpg")
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    chosen_contours = []
+    for j in range(len(contours)):
+        if hierarchy[0, j, 3] == -1:#no parent
+            if hierarchy[0, j, 2] !=-1: #child
+                print("no parent, child present")
+                chosen_contours.append(contours[j])
+
+    return chosen_contours  
+
+def extractInfoFromTag(tag):
+
+    tag_size = tag.shape[0]
+    grid_size = 8
+    pixels_in_one_grid =  int(tag_size/8)
+
+    info_with_padding = np.zeros((8,8))
+
+    for i in range(0, grid_size, 1):
+        for j in range(0, grid_size, 1):
+            grid = tag[i*pixels_in_one_grid:(i+1)*pixels_in_one_grid, j*pixels_in_one_grid:(j+1)*pixels_in_one_grid]
+            info_with_padding[i,j] = np.median(grid)
+    info = info_with_padding[2:6, 2:6]
+    return info
+
+def decipherInfoFromTag(info):
+    while not info[3,3]:
+        info = np.rot90(info, 1)
+    id_info = info[1:3, 1:3]
+    id_info_flat = np.array([id_info[0,0], id_info[0,1], id_info[1,1], id_info[1,0]])
+    tag_id = 0
+    for i in range(4):
+        if(id_info_flat[i]):
+            tag_id = tag_id + 2**i
+    return tag_id
+
+def imageBlur(input_image, SavePath, i):
+
+    image = input_image.copy()
+    #fft
+    fft = scipy.fft.fft2(image, axes = (0,1))
+    fft_shifted = scipy.fft.fftshift(fft)
+    magnitude_spectrum_fft_shifted = 20*np.log(np.abs(fft_shifted))
+
+    #fft+mask
+    fft_masked = fft_shifted * createGaussianMask(image.shape, 30, 30)
+    magnitude_spectrum_masked= 20*np.log(np.abs(fft_masked))
+
+    #image back
+    img_back = scipy.fft.ifftshift(fft_masked)
+    img_back = scipy.fft.ifft2(img_back)
+    img_back = np.abs(img_back)
+
+    fx, plts = plt.subplots(2,2,figsize = (10,5))
+    plts[0][0].imshow(image, cmap = 'gray')
+    plts[0][1].imshow(magnitude_spectrum_fft_shifted, cmap = 'gray')
+    plts[1][0].imshow(magnitude_spectrum_masked, cmap = 'gray')
+    plts[1][1].imshow(img_back, cmap = 'gray')
+
+    plt.savefig(SavePath + "fft" + str(i) + ".jpg")
+
+    return img_back
+
+
+def findTag(chosen_frame, SavePath, i):
+
+    image = chosen_frame.copy()
+    image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    image_color = chosen_frame.copy()
+
+    image_blur = imageBlur(image_gray, SavePath, i)
+    chosen_contours = findContour(image_blur, SavePath, i)
+
+    out_mask = np.zeros_like(image_gray)
+    corners = []
+    for chosen_contour in chosen_contours:
+        corner = cv2.approxPolyDP(chosen_contours[0], 0.009 * cv2.arcLength(chosen_contours[0], True), True) 
+        corners.append(corner.reshape(-1,2))
+        cv2.drawContours(image_color, chosen_contour, -1, (0, 0 ,255), 3)
+        cv2.drawContours(out_mask, [chosen_contour], -1, 1, cv2.FILLED)    
+
+
+    out_mask_mul = np.dstack((out_mask, out_mask, out_mask))
+    detected_april_tag = image_color * out_mask_mul
+    for c in corners:
+        for r in range(c.shape[0]):
+            x,y = c[r,:]
+            cv2.circle(detected_april_tag,(x,y),20,(255, 0, 255),-1) 
+    plt.figure()
+    plt.imshow(image_color)
+    plt.savefig(SavePath + "contour" + str(i) + ".jpg")
+
+    fx, plts = plt.subplots(1,2,figsize = (10,7))
+    plts[0].imshow(out_mask, cmap = 'gray')
+    plts[1].imshow(detected_april_tag)
+    plt.savefig(SavePath + "detect" + str(i) + ".jpg")
+
+    size_x = 400
+    size_y = 300
+
+    corner1 = corners[0]
+    corner2 = np.array([[0, 0], [0, size_y], [size_x, size_y], [size_x, 0]])
+
+    H = computeHomography(np.float32(corner1), np.float32(corner2))
+    image_transformed = applyHomography2ImageUsingInverseWarping(image_gray, H, (size_x, size_y))
+    plt.figure()
+    plt.imshow(image_transformed, cmap = 'gray')
+    plt.savefig(SavePath + "img_trans" + str(i) + ".jpg")
+
+    ret,thresh = cv2.threshold(np.uint8(image_transformed), 200 ,255,cv2.THRESH_BINARY)
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    chosen_contours = []
+    print(len(contours))
+    print(hierarchy)
+    for j in range(len(contours)):
+        if hierarchy[0, j, 3] == 0:#no parent
+            if hierarchy[0, j, 2] !=-1: #child
+                print("no parent, child present")
+                chosen_contours.append(contours[j])
+
+    tag_size = 160
+    tag_corner = cv2.approxPolyDP(chosen_contours[0], 0.009 * cv2.arcLength(chosen_contours[0], True), True) 
+    desired_corner = np.array([[0,0], [tag_size, 0], [tag_size, tag_size], [0, tag_size]])
+
+    H = computeHomography(np.float32(tag_corner.reshape(4,2)), np.float32(desired_corner))
+    tag = applyHomography2ImageUsingInverseWarping(image_transformed, H, (tag_size, tag_size))
+    ret,tag = cv2.threshold(tag, 200 ,255,cv2.THRESH_BINARY)
+    plt.figure()
+    plt.imshow(tag, cmap = 'gray')
+    plt.savefig(SavePath + "tag" + str(i) + ".jpg")
+
+    return tag
+
+
+def main():
+    BasePath = '/home/sakshi/courses/ENPM673/sakshi_p1/'
+    SavePath = BasePath + "Results/problem1/"
+    video_file = BasePath + "Data/Tag2.mp4"
+    cap = cv2.VideoCapture(video_file)
+
+    frame_index = 2
+    i = 0
+    while(True):
+        ret, frame = cap.read()
+        if not ret:
+            print("Stream ended..")
+            break
+        i = i+1
+        if i == frame_index:
+            chosen_frame = frame
+        
+        cv2.imshow('frame',frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+    tag = findTag(chosen_frame, SavePath, frame_index)
+    info = extractInfoFromTag(tag)
+    tag_id = decipherInfoFromTag(info)
+    print("The tag ID is ", tag_id)
+
+if __name__ == '__main__':
+    main()
---
+++ Code/Utils/Utils.py
@@ -0,0 +1,88 @@
+import numpy as np
+
+def computeHomography(corners1, corners2):
+
+    if (len(corners1) < 4) or (len(corners2) < 4):
+        print("Need atleast four points to compute SVD.")
+        return 0
+
+    x = corners1[:, 0]
+    y = corners1[:, 1]
+    xp = corners2[:, 0]
+    yp = corners2[:,1]
+
+    nrows = 8
+    ncols = 9
+    
+    A = []
+    for i in range(int(nrows/2)):
+        row1 = np.array([-x[i], -y[i], -1, 0, 0, 0, x[i]*xp[i], y[i]*xp[i], xp[i]])
+        A.append(row1)
+        row2 = np.array([0, 0, 0, -x[i], -y[i], -1, x[i]*yp[i], y[i]*yp[i], yp[i]])
+        A.append(row2)
+
+    A = np.array(A)
+    U, E, VT = np.linalg.svd(A)
+    V = VT.transpose()
+    H_vertical = V[:, V.shape[1] - 1]
+    H = H_vertical.reshape([3,3])
+    H = H / H[2,2]
+    # print("the Homography matrix is")
+    # print(H)
+    return H
+
+def applyHomography2Image(image, H, size):
+    h, w = image.shape[:2] 
+    Yi, Xi = np.indices((h, w)) 
+    lin_homg_pts = np.stack((Xi.ravel(), Yi.ravel(), np.ones(Xi.size)))
+
+    trans_lin_homg_pts = H.dot(lin_homg_pts)
+    trans_lin_homg_pts /= trans_lin_homg_pts[2,:]
+
+    X_min = np.min(trans_lin_homg_pts[0,:])
+    Y_min = np.min(trans_lin_homg_pts[1,:])
+    X_max = np.max(trans_lin_homg_pts[0,:])
+    Y_max = np.max(trans_lin_homg_pts[1,:])
+
+    image_transformed = np.zeros((int(Y_max - Y_min) + 1, int(X_max - X_min) + 1))
+    x, y = trans_lin_homg_pts[:2,:].astype(int)
+    image_transformed[y, x] = image[Yi.ravel(), Xi.ravel()]
+    image_cropped = image_transformed[0:size[1], 0:size[0]]
+
+    return image_cropped
+
+
+def applyHomography2ImageUsingInverseWarping(image, H, size):
+
+    Xt, Yt = np.indices((size[0], size[1]))
+    lin_homg_pts_trans = np.stack((Xt.ravel(), Yt.ravel(), np.ones(Xt.size)))
+
+    H_inv = np.linalg.inv(H)
+    lin_homg_pts = H_inv.dot(lin_homg_pts_trans)
+    lin_homg_pts /= lin_homg_pts[2,:]
+
+    X_min = np.min(lin_homg_pts[0,:]).astype(int)
+    Y_min = np.min(lin_homg_pts[1,:]).astype(int)
+    X_max = np.max(lin_homg_pts[0,:]).astype(int)
+    Y_max = np.max(lin_homg_pts[1,:]).astype(int)
+    X_min, X_max, Y_min, Y_max
+
+    #pad image
+    max_val = np.max([X_max, image.shape[0], Y_max, image.shape[1]])
+    image_i = np.zeros((max_val, max_val))
+    image_i[0:image.shape[0], 0:image.shape[1]] = image
+
+    image_transformed = np.zeros((size[0], size[1]))
+    Xi, Yi = lin_homg_pts[:2,:].astype(int)
+    image_transformed[Xt.ravel(), Yt.ravel()] = image_i[Yi, Xi]
+    
+    return image_transformed
+
+def createGaussianMask(image_size, sigma_x, sigma_y):
+    cols, rows = image_size
+    centre_x, centre_y = rows / 2, cols / 2
+    x = np.linspace(0, rows, rows)
+    y = np.linspace(0, cols, cols)
+    X, Y = np.meshgrid(x,y)
+    mask = np.exp(-(np.square((X - centre_x)/sigma_x) + np.square((Y - centre_y)/sigma_y)))
+    return mask
